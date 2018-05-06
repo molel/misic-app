fa7a7e8c2fd8234f7c0ac8814170ae90985e74bc
molel <33999966+molel@users.noreply.github.com>
2018-05-06 12:23:27 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
+import store from './store';
+import { Provider } from 'mobx-react'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}>
+    <App />
+</Provider>, document.getElementById('root'));
