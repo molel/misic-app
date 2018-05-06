df7bf73fd994215db736badaed1de450d38ffaa8
molel <33999966+molel@users.noreply.github.com>
2018-05-06 12:24:35 +0300

    Update index.js

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,37 +1,36 @@
 import React from 'react'
-import InfoView from '../InfoView';
 import GridView from '../GridView';
-import TableView from '../TableView';
+import InfoView from '../InfoView';
 import SearchView from '../SearchView';
+import TableView from '../TableView';
 import Menu from '../../components/Menu';
 
-export default class App extends React.Component{
-    /*
-    *page: grid, table, info, search
-    */
-    state = {page: 'grid'}
-    render(){
+export default class App extends React.Component {
+    /**
+     * page: grid, info, table, search
+     */
+    state = { page: 'grid' }
+    render() {
         let content
-        switch (this.state.page){
+        switch (this.state.page) {
             case 'grid':
-            content=<GridView/>
-            break;
+                content = <GridView />
+                break
             case 'info':
-            content=<InfoView/>
-            break;
-            case 'search':
-            content=<SearchView/>
-            break;
+                content = <InfoView />
+                break
             case 'table':
-            content=<TableView/>
-            break;
+                content = <TableView />
+                break
+            case 'search':
+                content = <SearchView />
+                break
             default:
-            content=<GridView/>
-            break;
+                content = <GridView />
         }
-        return(<React.Fragment>
-        <Menu />
-        {content}
+        return (<React.Fragment>
+            <Menu />
+            {content}
         </React.Fragment>)
     }
 }
